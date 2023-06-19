51d8befbefdbd7c2d7538c1981dc3f037702ebf9
santacoll19 <collazokel19@gmail.com>
2023-06-19 19:30:09 +0000

    positivo negativo

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This program will assign a random number
+ * n: will be variable
+ * Return:  Always return 1
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is greater than 0\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is less than 0\n", n);
+	return (0);
+}
