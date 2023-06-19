010d5b895c63c1efe7613c0d9401ee1ba2f43f0a
santacoll19 <collazokel19@gmail.com>
2023-06-19 19:35:27 +0000

    positivo negativo

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is greater 0\n", n);
+		printf("%d is positive 0\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
-		printf("%d is less 0\n", n);
+		printf("%d is negative 0\n", n);
 	return (0);
 }
