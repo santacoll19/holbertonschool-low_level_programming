6f019f7c0032ddaa458f88699e89a533fab50124
santacoll19 <collazokel19@gmail.com>
2023-06-20 16:02:32 +0000

    todo menos q y e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- Alphabet except q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++) 
+        if (c != 'q' && c != 'e') 
+            putchar(c);
+	putchar ('\n');
+
+	return (0);
+}
