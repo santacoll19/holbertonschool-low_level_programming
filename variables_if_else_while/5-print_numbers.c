8003e16f238ac5e959545e460f18bb0cbdf9dd91
santacoll19 <collazokel19@gmail.com>
2023-06-20 17:26:41 +0000

    print de 0 a 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- prints all single digit numbers
+ *
+ * Return: Always (0)
+ */
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
+
