b9aaa170a138f0ad2aa9b57ae382b72f3b832c39
santacoll19 <collazokel19@gmail.com>
2023-06-20 14:27:43 +0000

    ultimo digito

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main-
+ * main-Last Digit
  * n: will store different value
  * Return: Always return 1
  */
@@ -18,10 +18,10 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if (n % 10 == 0)
-	{	
+	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
