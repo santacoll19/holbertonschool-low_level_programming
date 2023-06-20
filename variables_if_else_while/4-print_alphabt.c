6d5a1070d407cfcf851d3bcb2603a604ab7745fc
santacoll19 <collazokel19@gmail.com>
2023-06-20 16:06:25 +0000

    todo menos q y e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,9 @@
 int main(void)
 {
 	char c;
-	for (c = 'a'; c <= 'z'; c++)
+	for(c = 'a'; c <= 'z'; c++)
 
-        if (c != 'q' && c != 'e');
+        if(c != 'q' && c != 'e')
             putchar(c);
 	putchar ('\n');
 
