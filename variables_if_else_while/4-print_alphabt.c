1ee975903be76fd45d768854d7e67e7e0c7c273d
santacoll19 <collazokel19@gmail.com>
2023-06-20 16:12:33 +0000

    alfabeto menos q/e

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,10 +10,12 @@
 int main(void)
 {
 	char c;
-	for(c = 'a'; c <= 'z'; c++)
 
-        if(c != 'q' && c != 'e')
-            putchar(c);
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			putchar (c);
+	}
 	putchar ('\n');
 
 	return (0);
