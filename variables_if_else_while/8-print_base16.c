b6935f7961a470fc06355774d297f7caa86adf08
santacoll19 <collazokel19@gmail.com>
2023-06-20 23:25:14 +0000

    alfabeto y numeros

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main-all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	char m;
+
+	for (n = 0; n < 10; n++)
+		putchar(n % 10);
+
+	for (m = 'a'; m <= 'f'; m++)
+		putchar(m);
+
+			putchar ('\n');
+	return (0);
+}
