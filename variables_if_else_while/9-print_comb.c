6b59905efe3d70218412507c257bd43638e2f035
santacoll19 <collazokel19@gmail.com>
2023-06-21 14:25:16 +0000

    horizontal num

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,16 +11,16 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n < 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 
-			if (n < 9)
+		if (n < '9')
 		{
 			putchar (',');
-			putchar(32);
+			putchar(' ');
 		}
-		putchar ('\n');
 	}
+	putchar ('\n');
 	return (0);
 }
