5ac2c6babecd06027fa5c258b331556f8a000931
santacoll19 <collazokel19@gmail.com>
2023-06-21 23:59:06 +0000

    lowcase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower- check the code.
+ *
+ * Return: Always 0.
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
