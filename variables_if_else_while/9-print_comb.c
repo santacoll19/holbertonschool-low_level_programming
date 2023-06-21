2a5613b0e1b00b4626f07bf2e9d9b39af126bf16
santacoll19 <collazokel19@gmail.com>
2023-06-21 14:07:00 +0000

    all possible combinations

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main- all possible combinations of single-digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 9; n++)
+	{
+		putchar(n);
+
+			if (n < 9)
+		{
+			putchar (',');
+			putchar(32);
+		}
+		putchar ('\n');
+	}
+	return (0);
+}
