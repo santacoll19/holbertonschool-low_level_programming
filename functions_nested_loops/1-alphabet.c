0f81c1da2aa950e5929bfcf6bc61c71222184566
santacoll19 <collazokel19@gmail.com>
2023-06-21 18:24:57 +0000

    prototipo

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *void print_alphabet - prints the alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	 int  n;
+	for (n = 'a'; n <= 'z'; n++)
+		{
+		_putchar(n);
+		}
+	_putchar('\n');
+}
