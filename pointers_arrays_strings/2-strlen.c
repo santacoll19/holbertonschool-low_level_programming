265435c35467ac06dd9d232c2e93822d6dfc675d
santacoll19 <collazokel19@gmail.com>
2023-06-26 18:47:51 +0000

    returns the length

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *strlen - check the code
+ *_strlen - check the code
  *@s: variable
  * Return: Always 0.
  */
@@ -11,7 +11,7 @@ int _strlen(char *s)
 {
 	int c;
 
-	for (c = 0 ; *s != 0 ; c++)
+	for (c = 0 ; *s != '\0' ; c++)
 		;
 
 	return (c);
