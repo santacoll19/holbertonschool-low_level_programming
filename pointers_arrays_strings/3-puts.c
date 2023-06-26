7898637fc0d95a088e14bd41fe370d955c0a9987
santacoll19 <collazokel19@gmail.com>
2023-06-26 22:42:13 +0000

    while *str

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_puts- check the code
+ @*str: variable
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
+
