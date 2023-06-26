3dba1c550949642899490fe1936d61f7740aa186
santacoll19 <collazokel19@gmail.com>
2023-06-26 17:52:40 +0000

    swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap the code
+ *@a: variables
+ *@b: variables
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+
+	*a = *b;
+
+	*b = c;
+}
