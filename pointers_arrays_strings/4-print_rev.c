cc64739f6510380c98bae002ac3d8785af2591c9
santacoll19 <collazokel19@gmail.com>
2023-06-27 17:44:45 +0000

    reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,19 @@
-include <stdio.h>
+#include <stdio.h>
 #include "main.h"
-
-/*
- *print_rev- check code.
- @s:variable
+/**
+ *print_rev - check code.
+ *@s:variable
  */
 
 void print_rev(char *s)
 {
 	int c = 0;
-	char *end = s;
 
-	while (*end != '\0')
-	{
+	for (c = 0; s[c] != '\0';)
 		c++;
-		end++;
-	}
 
-	for (int i = c - 1; i >= 0; i--)
-	{
-		putchar(s[i]);
-	}
-	putchar('\n');
+	while (c)
+		_putchar(s[--c]);
+
+	_putchar('\n');
 }
