d71769e8e2538acd3e637bb7d29d3e857a85a202
santacoll19 <collazokel19@gmail.com>
2023-07-07 14:27:13 +0000

    multiplicacion

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - program that multiplies two numbers
+ * @argc: counts the arguments
+ * @argv: string that hold the arguments
+ * Return: return 0 passed
+ */
+int main(int argc, char *argv[])
+{
+	int i, b, a;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		a = atoi(argv[2]);
+		b = a * i;
+
+		printf("%d\n", b);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (0);
+}
