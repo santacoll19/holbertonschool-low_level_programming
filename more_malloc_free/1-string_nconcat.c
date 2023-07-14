bd8e7f43440a7cb4f8b4dd4b240bde459bfbbe3b
santacoll19 <collazokel19@gmail.com>
2023-07-14 20:19:30 +0000

    string

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -20,36 +20,29 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	/* Check if the second string is NULL. If it is, set it to an empty string */
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	/* Calculate the length of the first and second string */
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-
 	/* Check if n is greater than the length of s2 */
 	if (n > len2)
 	{
 		n = len2;
 	}
-
 	/* Allocate a memory block for the concatenated string */
 	new_str = malloc(len1 + n + 1);
-
 	/*Check if the memory allocation was successful. If it wasn't, return NULL*/
 	if (new_str == NULL)
 	{
 		return (NULL);
 	}
-
 	/* Copy the contents of the first and second string to the new memory block */
 	memcpy(new_str, s1, len1);
 	memcpy(new_str + len1, s2, n);
-
 	/* terminated the new str with a NULL char */
 	new_str[len1 + n] = '\0';
 	return (new_str);
