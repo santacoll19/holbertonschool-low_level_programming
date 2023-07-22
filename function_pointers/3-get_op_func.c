326b522e3d3dfa62aa2f61c2cb8e0a3237ec4809
santacoll19 <collazokel19@gmail.com>
2023-07-22 14:32:18 +0000

    3num

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ *get_op_func- gets input
+ *@s: stores variable
+ *Return: resultados
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	int i = 0;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
