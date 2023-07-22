1f6d1025370f3011e2d73392a65dd1af4bc0d405
santacoll19 <collazokel19@gmail.com>
2023-07-22 15:03:49 +0000

    print string

---
+++ variadic_functions/2-print_string.c
@@ -0,0 +1,32 @@
+#include "variadic_function.h"
+
+/*
+ *print_string- gets input
+ *@separator: stores char entry
+ *@n: stores unsigned int entry
+ */
+
+void print_string(const char *separator, const unsigned int n, ...)
+{
+	char a;
+	unsigned int i;
+	va_list word;
+
+	va_start(word, n);
+
+	for (i = 0; i < n; i++)
+	{
+		a = va_arg(word, char);
+
+		if (a == NULL)
+		{
+			a = "(nil)";
+		}
+		printf("%s", a);
+		if (i < (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		printf("\n");
+		va_end(word);
+}
