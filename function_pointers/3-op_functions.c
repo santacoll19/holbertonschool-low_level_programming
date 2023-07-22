8d4c0592a190d198435ffcabf0fd89c8270fb8e0
santacoll19 <collazokel19@gmail.com>
2023-07-22 14:25:22 +0000

    num3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ *op_add- gets users input
+ *@a: stores first variable
+ *@b: stores second variable
+ *Return: results
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *op_sub- gets users input
+ *@a: stores first variable
+ *@b: stores second variable
+ *Return: results
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ *op_mul- gets users input
+ *@a: stores first variable
+ *@b: stores second variable
+ *Return: results
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ *op_div- gets users input
+ *@a: stores first variable
+ *@b: stores second variable
+ *Return: results
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ *op_mod- gets users input
+ *@a: stores first variable
+ *@b: stores second variable
+ *Return: results
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
