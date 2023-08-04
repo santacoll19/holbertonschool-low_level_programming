ee1e48421575bb3287e25bb0c8df119e47a6f7bb
santacoll19 <collazokel19@gmail.com>
2023-08-04 10:53:02 -0400

    hot fix

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -21,9 +21,9 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 /* prototype needed for the project */
