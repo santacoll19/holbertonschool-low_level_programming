1d3fe20487ed0acee3abcc49a8165916cde14f87
santacoll19 <collazokel19@gmail.com>
2023-08-04 20:22:54 -0400

    fix

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * print_dlistint- gets input.
- * @h: stores input
- * Return: results.
-*/
+ *print_dlistint- gets input
+ *@h: stores input
+ *Return: results
+ */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-size_t count = 0;
+	size_t count = 0; 
 
-while (h != NULL)
-{
- printf("%d\n", h->n);
- h = h->next;
-count ++;
-}
-return (count);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+   
 }
